ecc147f5e363114f86810919c2d2e837500006c9
maskerade <stefdefig@hotmail.co.uk>
2020-09-08 22:37:37 +0100

    fix: entrypoint cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,26 +6,24 @@ if [ "${INPUT_CFN_SUBDIRECTORY}" == "" ]; then
     exit 1
 fi
 
-
-# Cleanup old templates
-rm ${PWD}/cfn_templates
-
 # List templates
-grep -r --exclude="entrypoint.sh" 'AWSTemplateFormatVersion' ${INPUT_CFN_SUBDIRECTORY}/* |cut -d':' -f1 > /tmp/cfn_templates
+grep -r --exclude="entrypoint.sh" 'Resources' ${INPUT_CFN_SUBDIRECTORY}/* |cut -d':' -f1 > /tmp/cfn_templates
 
-if [-d ] ${PWD}/reports ; then
-    echo "cleaning up existing report files"
-    rm ${PWD}/reports/results*.json
+if [[ -d "${PWD}/reports" ]] ; then
+    echo "Cleaning up existing report files"
+    rm ${PWD}/reports/results*.txt
 else
     echo "Creating ${PWD}/reports"
     mkdir ${PWD}/reports
 fi
 
+count=0
+
 for x in $(cat /tmp/cfn_templates); do
     echo "Scanning file - ${x}"
     echo "Scanning file - ${x}" > ${PWD}/reports/results${count}.txt
     echo "Scanning datetime - "$(date +%Y%m%d_%H%M%S)  >> ${PWD}/reports/results${count}.txt
-    cfn-guard check --strict-checks --rule_set /rules/cfn-guard-sqs.ruleset  --template ${x} >> ${PWD}/reports/results${count}.txt
+    ./cfn-guard check --strict-checks --rule_set ${PWD}/rules/cfn-guard-sqs.ruleset  --template ${PWD}/${x} >> ${PWD}/reports/results${count}.txt
     count=`expr $count + 1`
 done
 cat reports/results*.txt > consolidated_report.txt
